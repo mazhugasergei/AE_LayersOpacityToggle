0e9509576560e9ca852b827570d14779a4185ced
Markus Wedler <markuswedler8@gmail.com>
2023-04-07 18:15:34 +1000

    Comments fixes

--- v2/listAllLayers.jsx
+++ v2/listAllLayers.jsx
@@ -1,16 +1,16 @@
 function listAllLayers(comp, level, tabSymbol){
-  // group with comp name & comp layers
+  // comp name & comp layers group
   var compGroup = win1.add("group")
   compGroup.orientation = "column"
   compGroup.alignment = ["left", "center"]
   var layers = comp.layers
-  // group with comp name
+  // comp name group
   var compNameGroup = compGroup.add("group")
   compNameGroup.alignment = ["left", "center"]
   for(var j = 0; j < level; j++) compNameGroup.add("statictext", undefined, tabSymbol)
   var compName = compNameGroup.add("statictext", undefined, comp.name)
   compName.alignment = ["left", "center"]
-  // group with comp layers
+  // comp layers group
   for(var i = 1; i <= layers.length; i++){
     var layer = layers[i]
     checkboxesLayers.push(layer)
