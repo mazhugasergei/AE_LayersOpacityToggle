9cdb03675c1c5ec55405c379565097146f4b9a58
Markus Wedler <markuswedler8@gmail.com>
2023-04-17 12:52:59 +1000

    Style the list

--- v3/createLayersTree.jsx
+++ v3/createLayersTree.jsx
@@ -1,6 +1,6 @@
 function createLayersTree(parentWindow, comp, level){
   // tab symbol shows how deep the leyer is nested
-  var tabSymbol = "-"
+  var tabSymbol = " "
   
   // default values (if these arguments were not provided)
   level = typeof level !== 'undefined' ? level : 0
@@ -11,25 +11,41 @@ function createLayersTree(parentWindow, comp, level){
   compGroup.orientation = "column"
   compGroup.alignment = ["left", "center"]
 
-  // a group with tab symbols and composition name
+  // a group with composition name
   var compNameGroup = compGroup.add("group")
   compNameGroup.alignment = ["left", "center"]
-  for(var i=0; i<level; i++) compNameGroup.add("statictext", undefined, tabSymbol)
-  var compName = compNameGroup.add("statictext", undefined, comp.name)
-  compName.alignment = ["left", "center"]
+  for(var i=0; i<level-1; i++) compNameGroup.add("statictext", undefined, tabSymbol)
+  if(level){
+    var expandButton = compNameGroup.add("statictext", undefined, ">")
+    expandButton.onClick = function(){
+      alert(1)
+    }
+    var compCheckbox = compNameGroup.add("checkbox")
+    compCheckbox.text = comp.name
+    checkboxes.push(compCheckbox) // global
+  }
+  else{
+    var compName = compNameGroup.add("statictext", undefined, comp.name)
+    compName.alignment = ["left", "center"]
+  }
 
   // a group with nested layers
   var layers = comp.layers
   for(var i=1; i<=layers.length; i++){
     var layer = layers[i]
     checkboxesLayers.push(layer) // global
-    var layerGroup = compGroup.add("group")
-    layerGroup.alignment = ["left", "center"]
-    for(var j=0; j<level; j++) layerGroup.add("statictext", undefined, tabSymbol)
-    var checkbox = layerGroup.add("checkbox")
-    checkbox.text = layer.name
-    checkboxes.push(checkbox) // global
     if(layer instanceof AVLayer && layer.source instanceof CompItem)
-      createLayersTree(parentWindow, layer.source, level+1)
+      createLayersTree(compGroup, layer.source, level+1)
+    else{
+      var layerGroup = compGroup.add("group")
+      // layerGroup.visible = false
+      layerGroup.alignment = ["left", "center"]
+      for(var j=0; j<level; j++) layerGroup.add("statictext", undefined, tabSymbol)
+      var invisibleExpandButton = layerGroup.add("statictext", undefined, ">")
+      invisibleExpandButton.visible = false
+      var checkbox = layerGroup.add("checkbox")
+      checkbox.text = layer.name
+      checkboxes.push(checkbox) // global
+    }
   }
 }
