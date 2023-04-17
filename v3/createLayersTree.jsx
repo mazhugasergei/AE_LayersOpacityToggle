04e1024743cee0c0ba551d521e734c6cae5b9dac
Markus Wedler <markuswedler8@gmail.com>
2023-04-17 17:31:06 +1000

    Update UI

--- v3/createLayersTree.jsx
+++ v3/createLayersTree.jsx
@@ -1,6 +1,8 @@
 function createLayersTree(parentWindow, comp, level){
   // tab symbol shows how deep the leyer is nested
   var tabSymbol = " "
+  var expandSymbol = "⮕"
+  var expandedSymbol = "⬇"
   
   // default values (if these arguments were not provided)
   level = typeof level !== 'undefined' ? level : 0
@@ -14,34 +16,36 @@ function createLayersTree(parentWindow, comp, level){
   // a group with composition name
   var compNameGroup = compGroup.add("group")
   compNameGroup.alignment = ["left", "center"]
-  for(var i=0; i<level-1; i++) compNameGroup.add("statictext", undefined, tabSymbol)
+  for(var i=0; i<level; i++) compNameGroup.add("statictext", undefined, tabSymbol)
   if(level){
-    var expandButton = compNameGroup.add("statictext", undefined, ">")
+    var expandButton = compNameGroup.add("button", undefined, expandSymbol)
+    expandButton.size = [20, 20]
     expandButton.onClick = function(){
-      alert(1)
+      this.text = layersGroup.visible ? expandSymbol : expandedSymbol
+      layersGroup.visible = !layersGroup.visible
     }
     var compCheckbox = compNameGroup.add("checkbox")
     compCheckbox.text = comp.name
     checkboxes.push(compCheckbox) // global
   }
-  else{
-    var compName = compNameGroup.add("statictext", undefined, comp.name)
-    compName.alignment = ["left", "center"]
-  }
 
   // a group with nested layers
+  var layersGroup = compGroup.add("group")
+  layersGroup.orientation = "column"
+  layersGroup.alignment = ["left", "center"]
+  if(level) layersGroup.visible = false
   var layers = comp.layers
   for(var i=1; i<=layers.length; i++){
     var layer = layers[i]
     checkboxesLayers.push(layer) // global
     if(layer instanceof AVLayer && layer.source instanceof CompItem)
-      createLayersTree(compGroup, layer.source, level+1)
+      createLayersTree(layersGroup, layer.source, level+1)
     else{
-      var layerGroup = compGroup.add("group")
-      // layerGroup.visible = false
+      var layerGroup = layersGroup.add("group")
       layerGroup.alignment = ["left", "center"]
-      for(var j=0; j<level; j++) layerGroup.add("statictext", undefined, tabSymbol)
-      var invisibleExpandButton = layerGroup.add("statictext", undefined, ">")
+      for(var j=0; j<level+1; j++) layerGroup.add("statictext", undefined, tabSymbol)
+      var invisibleExpandButton = layerGroup.add("button", undefined, expandSymbol)
+      invisibleExpandButton.size = [20, 20]
       invisibleExpandButton.visible = false
       var checkbox = layerGroup.add("checkbox")
       checkbox.text = layer.name
